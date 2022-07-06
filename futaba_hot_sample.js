eca62e8758cdb83c6f25969ea7781150fc0fa527
magosa <nwtwh820@gmail.com>
2022-07-06 15:09:43 +0900

    bug fixed.

--- futaba_hot_sample.js
+++ futaba_hot_sample.js
@@ -14,19 +14,19 @@ const dir_conf = {
 
 async function main() {
   // トークンの発行・更新
-  await client.getAccessToken(obj)
-    .then(res => {
-      obj.access_token = res.accessToken;
-      obj.refresh_token = res.refreshToken;
-      if (obj.access_token && obj.refresh_token) {
-        fs.writeFileSync('./data/config.json', JSON.stringify(obj));
-      }
-      console.log(res);
-    });
+  // await client.getAccessToken(obj)
+  //   .then(res => {
+  //     obj.access_token = res.accessToken;
+  //     obj.refresh_token = res.refreshToken;
+  //     if (obj.access_token && obj.refresh_token) {
+  //       fs.writeFileSync('./data/config.json', JSON.stringify(obj));
+  //     }
+  //     console.log(res);
+  //   });
 
 
   //既存トークンのセット
-  // client.setAccessToken(obj);
+  client.setAccessToken(obj);
 
 
   /**
@@ -34,7 +34,7 @@ async function main() {
    */
 
   let r90 = new futility();
-  r90.setTargetBuilding("R90/research")
+  r90.setTargetBuilding("Shinsuna/TKCentralSouth")
     .setDownloadFolderPath(__dirname + '/download/');
 
   // 1.テレメトリ取得
@@ -50,7 +50,7 @@ async function main() {
     .setFilterOfDtId(["R90_000001", "R90_000002"])
     .generateParameterWithFilter();
 
-  //メソッドチェーンを使わない場合はコチラ
+  // メソッドチェーンを使わない場合はコチラ
   // const filter_data_1 = {
   //   "title": {
   //     "values": ["ElementA", "ElementB"]
@@ -72,9 +72,13 @@ async function main() {
   //     "values": ["R90_000001", "R90_000002"]
   //   }
   // }
-  // const request_body_1_3 = generateParameterWithFilter(filter_data_1)
+  const request_body_1_3 = generateParameterWithFilter(filter_data_1)
 
   const telemetry_search_parameters = request_body_1_1;
+
+  const telemetry_search_parameters = r90.initializeFilterObject()
+    .setFilterOfDtId(["TGX_002911"])
+    .generateParameterWithFilter();
 
   client.getTelemetryData(telemetry_search_parameters)
     .then(res => console.dir(res, dir_conf));
@@ -121,11 +125,11 @@ async function main() {
 
 
   // 4.ポイント遠隔制御
-  const control_dtid = "R90_000001";
-  const control_value = 26.2;
-  const control_priority = 40;
+  const control_dtid = "TGX_002923";
+  const control_value = 10;
+  const control_priority = 16;
 
-  client.controlDigitalTwinData("R90/research", control_dtid, control_value, control_priority)
+  client.controlDigitalTwinData("Shinsuna/TKCentralSouth", control_dtid, control_value, control_priority)
     .then(res => console.dir(res, dir_conf));
 
 
@@ -141,80 +145,80 @@ async function main() {
     .setFilterOfTwinModelId(["dtmi:point:bacnetPoint;1", "dtmi:point:humanPoint;1"])
     .setFilterOfDtId(["R90_000001", "R90_000002"])
     .generateParameterWithFilter();
-  const stream_registration_parameters = request_body_5_1;
+  let stream_registration_parameters = request_body_5_1;
   client.setTelemetryStream(stream_registration_parameters)
     .then(res => console.dir(res, dir_conf));
 
 
   // 6.ストリーミング解除
-  const model_6_1 = r90.generateSearchParameters("dtmi:point:bacnet", "startswith"));
-const request_body_6_1 = r90.generateParameterWithPath("/research_1FL/*", model_6_1);
-const request_body_6_2 = r90.generateParameterWithQuery("SELECT T FROM digitaltwins T WHERE IS_DEFINED(T.m_pointId)");
-const request_body_6_3 = r90.initializeFilterObject()
-  .setFilterOfTwinTitle(["ElementA", "ElementB"])
-  .setFilterOfGlobalId(["aaa", "bbb"])
-  .setFilterOfTwinTag(["tagA", "tagC"], "and")
-  .setFilterOfTwinPath(["/A/*", "/B"])
-  .setFilterOfTwinModelId(["dtmi:point:bacnetPoint;1", "dtmi:point:humanPoint;1"])
-  .setFilterOfDtId(["R90_000001", "R90_000002"])
-  .generateParameterWithFilter();
-const stream_registration_parameters = request_body_6_1;
-client.setTelemetryStream(stream_registration_parameters)
-  .then(res => console.dir(res, dir_conf));
+  const model_6_1 = r90.generateSearchParameters("dtmi:point:bacnet", "startswith");
+  const request_body_6_1 = r90.generateParameterWithPath("/research_1FL/*", model_6_1);
+  const request_body_6_2 = r90.generateParameterWithQuery("SELECT T FROM digitaltwins T WHERE IS_DEFINED(T.m_pointId)");
+  const request_body_6_3 = r90.initializeFilterObject()
+    .setFilterOfTwinTitle(["ElementA", "ElementB"])
+    .setFilterOfGlobalId(["aaa", "bbb"])
+    .setFilterOfTwinTag(["tagA", "tagC"], "and")
+    .setFilterOfTwinPath(["/A/*", "/B"])
+    .setFilterOfTwinModelId(["dtmi:point:bacnetPoint;1", "dtmi:point:humanPoint;1"])
+    .setFilterOfDtId(["R90_000001", "R90_000002"])
+    .generateParameterWithFilter();
+  stream_registration_parameters = request_body_6_1;
+  client.deleteTelemetryStream(stream_registration_parameters)
+    .then(res => console.dir(res, dir_conf));
 
 
-// 7.ストリーミング登録状況確認
-client.checkTelemetryStream()
-  .then(res => console.dir(res, dir_conf));
+  // 7.ストリーミング登録状況確認
+  client.checkTelemetryStream()
+    .then(res => console.dir(res, dir_conf));
 
 
-// 8.ストリーミング登録データ取得(仕様確認中)
-// client.getTelemetryStream(__dirname + "/data/telemetryStreamClient.proto")
-//   .then(res => {
-//     res.on("data", (data) => {
-//       console.dir(data.telemetries, dir_conf);
-//     })
-//   });
+  // 8.ストリーミング登録データ取得(仕様確認中)
+  // client.getTelemetryStream(__dirname + "/data/telemetryStreamClient.proto")
+  //   .then(res => {
+  //     res.on("data", (data) => {
+  //       console.dir(data.telemetries, dir_conf);
+  //     })
+  //   });
 
 
 
-/**
- * WoT API サンプル
- */
+  /**
+   * WoT API サンプル
+   */
 
-// 1.WoT TD取得
-client.getThings("R90/research/reasearch_1FL/*")
-  .then(res => console.dir(res, dir_conf));
-
-
-// 2.WoT TD取得
-const request_body_w_2_1 = r90.generateParameterWithQuery("SELECT * FROM digitaltwins T WHERE T.tag.TagA = true");
-const request_body_w_2_2 = r90.initializeFilterObject()
-  .setFilterOfTwinTitle(["ElementA", "ElementB"])
-  .setFilterOfGlobalId(["aaa", "bbb"])
-  .setFilterOfTwinTag(["tagA", "tagC"], "and")
-  .setFilterOfTwinPath(["/A/*", "/B"])
-  .setFilterOfTwinModelId(["dtmi:point:bacnetPoint;1", "dtmi:point:humanPoint;1"])
-  .setFilterOfDtId(["R90_000001", "R90_000002"])
-  .generateParameterWithFilter();
-const wot_search_parameters = request_body_w_2_1;
-client.getThingsByParameter(wot_search_parameters)
-  .then(res => console.dir(res, dir_conf));
+  // 1.WoT TD取得
+  client.getThings("R90/research/reasearch_1FL/*")
+    .then(res => console.dir(res, dir_conf));
 
 
-// 3.WoT Property取得 (TD内全取得)
-client.getThingsProperties(1, "398897d193674abf9200ac59ddc8c749")
-  .then(res => console.dir(res, dir_conf));
+  // 2.WoT TD取得
+  const request_body_w_2_1 = r90.generateParameterWithQuery("SELECT * FROM digitaltwins T WHERE T.tag.TagA = true");
+  const request_body_w_2_2 = r90.initializeFilterObject()
+    .setFilterOfTwinTitle(["ElementA", "ElementB"])
+    .setFilterOfGlobalId(["aaa", "bbb"])
+    .setFilterOfTwinTag(["tagA", "tagC"], "and")
+    .setFilterOfTwinPath(["/A/*", "/B"])
+    .setFilterOfTwinModelId(["dtmi:point:bacnetPoint;1", "dtmi:point:humanPoint;1"])
+    .setFilterOfDtId(["R90_000001", "R90_000002"])
+    .generateParameterWithFilter();
+  const wot_search_parameters = request_body_w_2_1;
+  client.getThingsByParameter(wot_search_parameters)
+    .then(res => console.dir(res, dir_conf));
 
 
-// 4.WoT Property取得 (1 Property)
-client.getThingsProperty(1, "398897d193674abf9200ac59ddc8c749", "Status")
-  .then(res => console.dir(res, dir_conf));
+  // 3.WoT Property取得 (TD内全取得)
+  client.getThingsProperties(1, "398897d193674abf9200ac59ddc8c749")
+    .then(res => console.dir(res, dir_conf));
 
 
-// 5.WoT Property書き込み
-client.setThingsProperty(1, "398897d193674abf9200ac59ddc8c749", "Control", 26.2, 40)
-  .then(res => console.dir(res, dir_conf));
+  // 4.WoT Property取得 (1 Property)
+  client.getThingsProperty(1, "398897d193674abf9200ac59ddc8c749", "Status")
+    .then(res => console.dir(res, dir_conf));
+
+
+  // 5.WoT Property書き込み
+  client.setThingsProperty(1, "398897d193674abf9200ac59ddc8c749", "Control", 26.2, 40)
+    .then(res => console.dir(res, dir_conf));
 
 }
 
